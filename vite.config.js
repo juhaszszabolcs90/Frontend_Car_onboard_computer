92c56dbd3f957ce54504bdf3b24c3996a4b83afd
Szabolcs Juhász <juhaszszabolcs90@gmail.com>
2025-01-11 10:22:27 +0100

    update system with docs

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/Frontend_Car_onboard_computer/',
+  build: {
+      outDir: 'docs',
+      emptyOutDir: true
+    },
   resolve: {
     alias: {
       '@': '/src' // Ez az alias biztosítja, hogy az @ a /src mappára hivatkozik
     }
   }
+  })
 
-})
